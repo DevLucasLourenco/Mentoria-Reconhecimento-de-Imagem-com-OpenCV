8e975c886811cca596528c1724330ae46b873f3a
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-12-11 22:48:17 -0300

    att

--- __init__.py
+++ __init__.py
@@ -42,13 +42,17 @@ class DeteccaoVisual:
                     for obj_Mao in lista_ObjMaos.multi_hand_landmarks:
                         os.system('cls')
                         
+                        inicio_dedo_polegar = obj_Mao.landmark[1]
+                        inicio_dedo_indicador = obj_Mao.landmark[5]
+                        inicio_dedo_medio = obj_Mao.landmark[9]
+                        
                         ponta_dedo_polegar = obj_Mao.landmark[4]
                         ponta_dedo_indicador = obj_Mao.landmark[8]
                         ponta_dedo_medio = obj_Mao.landmark[12]
                         ponta_dedo_anelar = obj_Mao.landmark[16]
                         ponta_dedo_mindinho = obj_Mao.landmark[20]
                         
-                        deteccao_gesto = DeterminarGestos(PDP=ponta_dedo_polegar, PDI=ponta_dedo_indicador, PDM=ponta_dedo_medio, PDA=ponta_dedo_anelar, PDMI=ponta_dedo_mindinho)
+                        deteccao_gesto = DeterminarGestos(IDP=inicio_dedo_polegar, IDI=inicio_dedo_indicador, IDM=inicio_dedo_medio, PDP=ponta_dedo_polegar, PDI=ponta_dedo_indicador, PDM=ponta_dedo_medio, PDA=ponta_dedo_anelar, PDMI=ponta_dedo_mindinho)
                         deteccao_gesto.identificar()
                         
                         DeteccaoVisual.DESENHO_MP.draw_landmarks(self.frameBGR, obj_Mao, DeteccaoVisual.RECONHECIMENTO_HANDS.HAND_CONNECTIONS)
@@ -67,9 +71,15 @@ class DeteccaoVisual:
 
 class DeterminarGestos:
     
-    def __init__(self, PDP, PDI, PDM, PDA, PDMI) -> None:
+    def __init__(self,IDP, IDI, IDM, PDP, PDI, PDM, PDA, PDMI) -> None:
         # Eixos -> {x:y}
         #----------------------------------------------------------------------
+        self.eixo_inicio_polegar:dict = self._tratar_obj_landmark(IDP)
+        self.eixo_inicio_indicador:dict = self._tratar_obj_landmark(IDI)
+        self.eixo_inicio_medio:dict = self._tratar_obj_landmark(IDM)
+        
+        #-----
+
         self.eixo_ponta_polegar:dict = self._tratar_obj_landmark(PDP)
         self.eixo_ponta_indicador:dict = self._tratar_obj_landmark(PDI)
         self.eixo_ponta_medio:dict = self._tratar_obj_landmark(PDM)
@@ -79,6 +89,12 @@ class DeterminarGestos:
         
         # Unpacking de X e Y dos Eixos
         #----------------------------------------------------------------------
+        self.X_inicio_polegar, self.Y_inicio_polegar = self.eixo_inicio_polegar.values()
+        self.X_inicio_indicador, self.Y_inicio_indicador = self.eixo_inicio_indicador.values()
+        self.X_inicio_medio, self.Y_inicio_medio = self.eixo_inicio_medio.values()
+        
+        #-----
+        
         self.X_ponta_polegar, self.Y_ponta_polegar = self.eixo_ponta_polegar.values()
         self.X_ponta_indicador, self.Y_ponta_indicador = self.eixo_ponta_indicador.values()
         self.X_ponta_medio, self.Y_ponta_medio = self.eixo_ponta_medio.values()
@@ -89,7 +105,7 @@ class DeterminarGestos:
         # Dict's Auxiliares
         #------------------------------
         self.delimitar_gesto_funcao:dict = {
-            'Pinça' : 'função pinça',
+            'Pinça' : 'print("Pinça")',
             'L' : 'função L',
             'V' : 'função V',
             'II' : 'função II',
@@ -99,20 +115,21 @@ class DeterminarGestos:
         
         self.logica_deteccao_movimento:dict = {
             ((self.Y_ponta_indicador - self.Y_ponta_polegar) > -0.05) and ((self.Y_ponta_indicador - self.Y_ponta_polegar) < -0.007) and ((self.Y_ponta_medio*1.50) < self.Y_ponta_indicador) : 'Pinça',
-            False : 'V',
+            (False) : 'L',
             }
         #------------------------------
         
-        # Testes para Cálculo
+        # Cálculo para Testes 
         #------------------------------
-        print(f'Polegar: {self.eixo_ponta_polegar}\n',
-              f'Indicador: {self.eixo_ponta_indicador}\n',
-              f'Médio: {self.eixo_ponta_medio}\n',
-              f'Anelar: {self.eixo_ponta_anelar}\n',
-              f'Mindinho: {self.eixo_ponta_mindinho}\n')
+        print(f'Polegar - Ponta: {self.eixo_ponta_polegar} - Inicio: {self.eixo_inicio_polegar} \n',
+              f'Indicador - Ponta: {self.eixo_ponta_indicador} - Inicio: {self.eixo_inicio_indicador} \n',
+              f'Médio - Ponta: {self.eixo_ponta_medio} - Inicio: {self.eixo_inicio_medio} \n',
+              f'Anelar - Ponta: {self.eixo_ponta_anelar}\n',
+              f'Mindinho - Ponta: {self.eixo_ponta_mindinho}\n')
 
 
-        print(math.sqrt((self.Y_ponta_indicador**2)+(self.Y_ponta_polegar**2)))
+        print('inicio e fim polegar: ', (self.X_ponta_polegar - self.X_inicio_polegar), ((self.X_ponta_polegar - self.X_inicio_polegar)  <= 0.2) and ((self.X_ponta_polegar - self.X_inicio_polegar) >= 0.125))
+        print('hipotenusa: ',math.sqrt((self.Y_ponta_indicador**2)+(self.Y_ponta_polegar**2)))
         #------------------------------
 
     
@@ -129,8 +146,7 @@ class DeterminarGestos:
         print(retorno_objeto_booleano_detectado)
         
         if retorno_objeto_booleano_detectado:
-            print(self.delimitar_gesto_funcao.get(retorno_objeto_booleano_detectado[0]))
-            # eval(self.delimitar_gesto_funcao.get(retorno_objeto_booleano_detectado[0]))
+            eval(self.delimitar_gesto_funcao.get(retorno_objeto_booleano_detectado[0]))
             
             
 if __name__ == "__main__":
